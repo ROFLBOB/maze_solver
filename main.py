024ef0bdc4beeab99b4b9969aac60de816998025
kingpoogie <bmadeley@getgoldn.com>
2025-03-29 23:11:08 -0400

    wall breaker added

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ from maze import Maze
 
 def main():
     win = Window(800, 600)
-    maze = Maze(25, 25, 10, 15, 50, 50, win)
+    num_rows = 12
+    num_cols = 16
+    margin = 50
+    screen_x = 800
+    screen_y = 600
+    cell_size_x = (screen_x- 2 * margin)/ num_cols
+    cell_size_y = (screen_y -2 * margin)/ num_rows
+    maze = Maze(margin, margin, num_rows, num_cols, cell_size_x, cell_size_y, win, 10)
 
     win.wait_for_close()
 
