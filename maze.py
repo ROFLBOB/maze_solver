6b879cf31b7550aa3d08806d660c3633be84444d
kingpoogie <bmadeley@getgoldn.com>
2025-04-08 23:06:55 -0400

    completed the maze solver

--- maze.py
+++ maze.py
@@ -132,7 +132,50 @@ class Maze():
 
     def _reset_cells_visited(self):
             #loop through the _cells[]
-            for i in range(self.num_cols-1):
-                for j in range(self.num_rows-1):
+            for i in range(self.num_cols):
+                for j in range(self.num_rows):
                     self._cells[i][j].visited = False
-                
+    
+    def solve(self):
+        return self._solve_r(0,0) #start at the entrance and return the result
+    
+    def _solve_r(self, i, j):
+        self._animate()
+
+        self._cells[i][j].visited = True
+        if i == self.num_cols - 1 and j == self.num_rows - 1:
+            return True
+        #down
+        if j < self.num_rows - 1 and not self._cells[i][j].has_bottom_wall and not self._cells[i][j+1].visited:
+            self._cells[i][j].draw_move(self._cells[i][j+1])
+            #recursively check this path
+            if self._solve_r(i, j+1):
+                return True
+            #if that didn't work, undo the move
+            self._cells[i][j].draw_move(self._cells[i][j+1], True) #True for Undo
+        #up
+        if j > 0 and not self._cells[i][j].has_top_wall and not self._cells[i][j-1].visited:
+            self._cells[i][j].draw_move(self._cells[i][j-1])
+            #recursively check this path
+            if self._solve_r(i, j-1):
+                return True
+            #if that didn't work, undo the move
+            self._cells[i][j].draw_move(self._cells[i][j-1], True) #True for Undo
+        #left
+        if i > 0 and not self._cells[i][j].has_left_wall and not self._cells[i-1][j].visited:
+            self._cells[i][j].draw_move(self._cells[i-1][j])
+            #recursively check this path
+            if self._solve_r(i-1, j):
+                return True
+            #if that didn't work, undo the move
+            self._cells[i][j].draw_move(self._cells[i-1][j], True) #True for Undo
+        #right
+        if i < self.num_cols - 1 and not self._cells[i][j].has_right_wall and not self._cells[i+1][j].visited:
+            self._cells[i][j].draw_move(self._cells[i+1][j])
+            #recursively check this path
+            if self._solve_r(i+1, j):
+                return True
+            #if that didn't work, undo the move
+            self._cells[i][j].draw_move(self._cells[i+1][j], True) #True for Undo
+
+        return False
